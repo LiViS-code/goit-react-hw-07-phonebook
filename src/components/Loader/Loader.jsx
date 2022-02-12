6a24af125ed972d9800c337d1ce58de33aecb044
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2022-02-12 14:16:40 +0200

    refactoring style loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,18 +1,18 @@
-import './Loader.css';
+import { BarsRow, BarsCol, Bars, Bar, Text } from './Loader.styled';
 
 export default function Loader() {
   return (
-    <div className="row">
-      <div className="col-sm-2">
-        <div id="bars1">
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-        <h5>Loading...</h5>
-      </div>
-    </div>
+    <BarsRow>
+      <BarsCol>
+        <Bars>
+          <Bar></Bar>
+          <Bar></Bar>
+          <Bar></Bar>
+          <Bar></Bar>
+          <Bar></Bar>
+        </Bars>
+      </BarsCol>
+      <Text>Loading...</Text>
+    </BarsRow>
   );
 }
